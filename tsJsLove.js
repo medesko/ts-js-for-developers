907f6dcb7299add473d1aa65850ebf531cc24e5f
Mohamed KEITA <medesko@gmail.com>
2021-08-12 11:54:02 +0200

    added example ts js runner

---
+++ tsJsLove.js
@@ -0,0 +1,3 @@
+var message = "I love JavaScript and TypeScript";
+console.log(message);
+// save the file as tsJsLove.ts 
---
+++ tsJsLove.ts
@@ -0,0 +1,3 @@
+let message: string = "I love JavaScript and TypeScript"; 
+console.log(message); 
+// save the file as tsJsLove.ts 

04e100bc883ddf85d65d4ef766802917e76c1f90
Mohamed KEITA <medesko@gmail.com>
2021-08-13 10:06:35 +0200

    format code

--- conditions/isPositive.ts
+++ conditions/isPositive.ts
@@ -1,11 +1,9 @@
 // Given two non-zero numbers a and b, create a function that checks if their product is positive without calculating it.
 
-const isPositive = (a: number, b: number) : boolean => {
-    // the left side represents the case where both a and b are positive
-    // the right side represents the case where both a and b are negative
-    // we return the test that one of them is true
-    return ((a > 0 && b > 0 ) || (a < 0 && b < 0))
-}
+// the left side represents the case where both a and b are positive
+// the right side represents the case where both a and b are negative
+// we return the test that one of them is true
+const isPositive = (a: number, b: number) : boolean => ((a > 0 && b > 0 ) || (a < 0 && b < 0))
 
 let positiveNum = 4
 let negativeNum = -6

abf5af566a1246cdfad6b8502d4490a94067753c
Mohamed KEITA <medesko@gmail.com>
2021-08-15 16:24:24 +0200

    Way 2  Without using a condition:

--- loops/printOdd1ToN.ts
+++ loops/printOdd1ToN.ts
@@ -12,6 +12,20 @@ const printOdd1ToN = (n: number) : string => {
 console.log(printOdd1ToN(13))
 console.log(printOdd1ToN(20))
 
+
+
+// // Way 2  Without using a condition:
+
+const printOdd1ToN2 = (n: number): string => {
+    let str = ''
+    for (let i = 1; i <= n; i += 2) 
+        str += i + ' '
+    return str
+}
+
+console.log(printOdd1ToN2(13))
+console.log(printOdd1ToN2(20))
+
 /*Output:
 1 3 5 7 9 11 13
 1 3 5 7 9 11 13 15 17 19

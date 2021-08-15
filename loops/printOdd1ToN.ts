002ead93aa2c8196c86b96e1dd4a4873b295559b
Mohamed KEITA <medesko@gmail.com>
2021-08-15 16:21:33 +0200

    Given a positive integer n, create a function that displays odd numbers from 1 to n.

---
+++ loops/printOdd1ToN.ts
@@ -0,0 +1,18 @@
+// Given a positive integer n, create a function that displays odd numbers from 1 to n.
+// Way 1 => By using a condition
+
+const printOdd1ToN = (n: number) : string => {
+    let str = ''
+    for (let i = 1; i <= n; i++)
+        if (i % 2 == 1) str += i + ' '
+
+    return str
+}
+
+console.log(printOdd1ToN(13))
+console.log(printOdd1ToN(20))
+
+/*Output:
+1 3 5 7 9 11 13
+1 3 5 7 9 11 13 15 17 19
+*/
